52e9396d02908e022b4230f9deaf948cd3026c7f
심현진 <hyunjin.dev143@gmail.com>
2025-03-14 21:46:46 +0900

    Refactor:아이템 조회 API 리팩토링

--- src/main/java/com/study/demo/controller/ItemController.java
+++ src/main/java/com/study/demo/controller/ItemController.java
@@ -3,21 +3,24 @@ package com.study.demo.controller;
 import com.study.demo.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //controller로 지정해주는 어노테이션
-@RequestMapping("/")
-@RequiredArgsConstructor
+@RequestMapping("/hello") //주소창에 localhost:port/hello 작성 시 매핑 됨
+@RequiredArgsConstructor //생성자 자동 생성(lombok)
 public class ItemController {
 
   private final ItemService itemService;
 
-  @GetMapping("/item")
-  public AbstractReadWriteAccess.Item getItem(@RequestBody AbstractReadWriteAccess.Item item) {
+  @GetMapping("/{item_id}")
+  public ItemDto getItem(@PathVariable Long itemId) {
+    //아이템 조회 API
+    return itemService.getItem(itemId);
+  }
 
-    return item;
+  @PostMapping("/item/{item}")
+  public AbstractReadWriteAccess.Item createItem(@PathVariable String item) {
+    //아이템 추가 API
+    return null;
   }
 }
