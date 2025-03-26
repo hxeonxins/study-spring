255329b5bc41768a9be22d3beedb60be34d811e8
심현진 <hyunjin.dev143@gmail.com>
2025-03-27 00:22:47 +0900

    Refactor: 반환 타입 ResponseEntity 수정

--- src/main/java/com/study/demo/controller/ItemController.java
+++ src/main/java/com/study/demo/controller/ItemController.java
@@ -3,9 +3,8 @@ package com.study.demo.controller;
 import com.study.demo.dto.ItemDto;
 import com.study.demo.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController //controller로 지정해주는 어노테이션
 @RequestMapping("/hello") //주소창에 localhost:port/hello 작성 시 매핑 됨
@@ -15,9 +14,11 @@ public class ItemController {
   private final ItemService itemService;
 
   @GetMapping("/get_item/{id}")
-  public Optional<ItemDto> getItem(@PathVariable Long id) {
+  public ResponseEntity<ItemDto> getItem(@PathVariable Long id) {
     //아이템 조회 API
-    return itemService.getItem(id);
+    return itemService.getItem(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
   }
 
 //  @PostMapping("/item/{item_id}")
